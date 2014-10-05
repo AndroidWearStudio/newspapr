aea6f04cfd83f91f43462421e51576ae02f5ae53
michaeljakob <michael@jakob.tv>
2014-10-05 15:16:44 +0200

    Added dummy text. Background blurry <3

--- app/src/main/java/com/troubi/newspapr/NewspaprActivity.java
+++ app/src/main/java/com/troubi/newspapr/NewspaprActivity.java
@@ -1,6 +1,9 @@
 package com.troubi.newspapr;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.ImageView;
@@ -23,6 +26,21 @@ public class NewspaprActivity extends Activity {
                 mImage = (ImageView) stub.findViewById(R.id.newspapr_image);
             }
         });
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(),
+                R.drawable.dummy_background);
+
+        String text = "Thank you, everyone. It’s great to see all of you. Welcome to Google I/O. Every year, we look forward to this date. We’ve been hard at work since last I/O evolving our platforms so that developers like you can build amazing experiences. So thank you for joining us in person. I/O is a pretty global event. We have viewing parties in over 597 locations in 85 countries, in six continents. And there are over one million people watching this on the live stream today. Let’s say hello to a few locations.";
+        startReadingActivity(text.replaceAll("\\s+", " ").split(" "), icon);
+    }
+
+    private void startReadingActivity(String[] text, Bitmap image) {
+        Intent reading = new Intent(this, ReadingActivity.class);
+        reading.putExtra(ReadingActivity.INTENT_EXTRA_TEXT, text);
+        reading.putExtra(ReadingActivity.INTENT_EXTRA_IMAGE, image);
+
+        startActivity(reading);
+
     }
 
 
--- app/src/main/java/com/troubi/newspapr/ReadingActivity.java
+++ app/src/main/java/com/troubi/newspapr/ReadingActivity.java
@@ -1,15 +1,52 @@
 package com.troubi.newspapr;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.support.wearable.view.WatchViewStub;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ReadingActivity extends Activity {
+public class ReadingActivity extends Activity implements Runnable {
+
+    public static final String INTENT_EXTRA_IMAGE = "image";
+    public static final String INTENT_EXTRA_TEXT = "text";
+
+    /**
+     * Word display time in milliseconds
+     */
+    private static final int WORD_DISPLAY_TIME = 200;
+
+    /**
+     * The first word is displayed a bit longer for orientation
+     */
+    private static final int STARTING_DELAY = WORD_DISPLAY_TIME * 4;
+
+    /**
+     * The highlighted char will be roughly at RELATIVE_HIGHLIGHT_POSITION * word.length
+     */
+    private static final double RELATIVE_HIGHLIGHT_POSITION = .3;
 
     private TextView mText;
     private ImageView mImage;
+
+    private String[] mTextArray;
+    private int mTextArrayPosition;
+
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +58,62 @@ public class ReadingActivity extends Activity {
             public void onLayoutInflated(WatchViewStub stub) {
                 mText = (TextView) stub.findViewById(R.id.reading_text);
                 mImage = (ImageView) stub.findViewById(R.id.reading_image);
+
+                mTextArray = getIntent().getExtras().getStringArray(INTENT_EXTRA_TEXT);
+
+                mText.setText(mTextArray[0]);
+                mTextArrayPosition = 0;
+                Bitmap image = (Bitmap) getIntent().getExtras().getParcelable(INTENT_EXTRA_IMAGE);
+                mImage.setImageBitmap(image);
+                mImage.setImageBitmap(createBlurImage());
+
+
+                mHandler.postDelayed(ReadingActivity.this, STARTING_DELAY);
             }
         });
+    }
+
+    private Bitmap createBlurImage() {
+        final Bitmap photo = ((BitmapDrawable) mImage.getDrawable())
+                .getBitmap().copy(Bitmap.Config.ARGB_8888, true);
+        final RenderScript rs = RenderScript.create(this);
+        final Allocation input = Allocation
+                .createFromBitmap(rs, photo, Allocation.MipmapControl.MIPMAP_NONE,
+                        Allocation.USAGE_SCRIPT);
+        final Allocation output = Allocation.createTyped(rs, input.getType());
+        final ScriptIntrinsicBlur script = ScriptIntrinsicBlur
+                .create(rs, Element.U8_4(rs));
+        script.setInput(input);
+        script.setRadius(25f);
+        script.forEach(output);
+        output.copyTo(photo);
+        return photo;
+    }
+
+    @Override
+    public void run() {
+        mTextArrayPosition++;
+
+        String word = mTextArray[mTextArrayPosition];
+        mText.setText(highlightPivotPosition(word));
+
+        if (mTextArrayPosition < mTextArray.length - 1) {
+            mHandler.postDelayed(this, WORD_DISPLAY_TIME);
+        }
+    }
+
+    private Spannable highlightPivotPosition(String text) {
+        SpannableStringBuilder word = new SpannableStringBuilder(text);
+
+        int highlightPosition = (int) Math.floor(text.length() * RELATIVE_HIGHLIGHT_POSITION);
+
+        word.setSpan(
+                new ForegroundColorSpan(Color.RED),
+                highlightPosition,
+                highlightPosition + 1,
+                SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+
+        return word;
     }
 }
