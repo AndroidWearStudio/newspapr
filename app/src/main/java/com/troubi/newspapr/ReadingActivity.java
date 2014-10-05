428584ce83f0776c749e7cd32046afab58ff643e
michaeljakob <michael@jakob.tv>
2014-10-05 13:55:33 +0200

    Added NewspaprActivity and NewspaprActivity

--- app/src/main/java/com/troubi/newspapr/ReadingActivity.java
+++ app/src/main/java/com/troubi/newspapr/ReadingActivity.java
@@ -6,24 +6,22 @@ import android.support.wearable.view.WatchViewStub;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class NewspaprActivity extends Activity {
+public class ReadingActivity extends Activity {
 
-    private TextView mTitle;
+    private TextView mText;
     private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_newspapr);
+        setContentView(R.layout.activity_reading);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTitle = (TextView) stub.findViewById(R.id.newspapr_title);
-                mImage = (ImageView) stub.findViewById(R.id.newspapr_image);
+                mText = (TextView) stub.findViewById(R.id.reading_text);
+                mImage = (ImageView) stub.findViewById(R.id.reading_image);
             }
         });
     }
-
-
 }
