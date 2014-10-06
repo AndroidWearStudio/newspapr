760791727f17c0cadf8a9994b1cae3be1c3c8a25
michaeljakob <michael@jakob.tv>
2014-10-06 06:36:07 +0200

    merged NewsPaprActivity

--- mobile/src/main/java/com/news/papr/NewsPaprActivity.java
+++ mobile/src/main/java/com/news/papr/NewsPaprActivity.java
@@ -12,6 +12,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
+import android.widget.Button;
 
 
 public class NewsPaprActivity extends Activity
@@ -34,6 +36,9 @@ public class NewsPaprActivity extends Activity
 
         LocalBroadcastManager.getInstance(this)
                 .registerReceiver(mMessageReceiver, new IntentFilter(BROADCAST_MSG));
+
+
+        loadContent();
     }
 
     private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
@@ -57,6 +62,47 @@ public class NewsPaprActivity extends Activity
 
         }
     }
+
+
+    private void loadContent() {
+        Button[] buttons = getCategoryButtons();
+
+
+        int i = 0;
+        for (Button b : buttons) {
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent reading = new Intent(NewsPaprActivity.this, ReadingActivity.class);
+                    // reading.putExtra(ReadingActivity.INTENT_EXTRA_TEXT, text[i]);
+
+
+                    startActivity(reading);
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                }
+            });
+            i++;
+        }
+    }
+
+
+    private Button[] getCategoryButtons() {
+        Button[] buttons = new Button[9];
+        buttons[0] = (Button) findViewById(R.id.category1);
+        buttons[1] = (Button) findViewById(R.id.category2);
+        buttons[2] = (Button) findViewById(R.id.category3);
+        buttons[3] = (Button) findViewById(R.id.category4);
+        buttons[4] = (Button) findViewById(R.id.category5);
+        buttons[5] = (Button) findViewById(R.id.category6);
+        buttons[6] = (Button) findViewById(R.id.category7);
+        buttons[7] = (Button) findViewById(R.id.category8);
+        buttons[8] = (Button) findViewById(R.id.category9);
+
+
+        return buttons;
+    }
+
+
 
     @Override
     protected void onStart() {
