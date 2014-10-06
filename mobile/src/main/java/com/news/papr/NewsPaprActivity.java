be98c3c3ca17d82b0e981a3dae6afd68ed8dea33
michaeljakob <michael@jakob.tv>
2014-10-06 06:28:59 +0200

    Add ReadingActivity

--- mobile/src/main/java/com/news/papr/NewsPaprActivity.java
+++ mobile/src/main/java/com/news/papr/NewsPaprActivity.java
@@ -6,8 +6,10 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.mariux.teleport.lib.TeleportClient;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -17,7 +19,9 @@ public class NewsPaprActivity extends Activity
     private static final String HELLO_WORLD_MOBILE_PATH = "/hello-world-mobile";
 
 
-    TeleportClient mTeleportClient;
+    private TeleportClient mTeleportClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,48 @@ public class NewsPaprActivity extends Activity
 
         //Connect the GoogleApiClient
         mTeleportClient = new TeleportClient(this);
+
+        loadContent();
     }
 
+    private void loadContent() {
+        Button[] buttons = getCategoryButtons();
+
+
+        int i = 0;
+        for (Button b : buttons) {
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent reading = new Intent(NewsPaprActivity.this, ReadingActivity.class);
+                    // reading.putExtra(ReadingActivity.INTENT_EXTRA_TEXT, text[i]);
+                    
+
+
+                    startActivity(reading);
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                }
+            });
+            i++;
+        }
+    }
+
+
+    private Button[] getCategoryButtons() {
+        Button[] buttons = new Button[9];
+        buttons[0] = (Button) findViewById(R.id.category1);
+        buttons[1] = (Button) findViewById(R.id.category2);
+        buttons[2] = (Button) findViewById(R.id.category3);
+        buttons[3] = (Button) findViewById(R.id.category4);
+        buttons[4] = (Button) findViewById(R.id.category5);
+        buttons[5] = (Button) findViewById(R.id.category6);
+        buttons[6] = (Button) findViewById(R.id.category7);
+        buttons[7] = (Button) findViewById(R.id.category8);
+        buttons[8] = (Button) findViewById(R.id.category9);
+
+
+        return buttons;
+    }
 
     /**
      * Send message to mobile handheld
---
+++ mobile/src/main/java/com/news/papr/Utility.java
@@ -0,0 +1,41 @@
+package com.news.papr;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.widget.ImageView;
+
+/**
+ * Created by michaeljakob on 10/5/14.
+ */
+public class Utility {
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public static Bitmap createBlurImage(Context ctx, Bitmap photo) {
+        final RenderScript rs = RenderScript.create(ctx);
+        final Allocation input = Allocation
+                .createFromBitmap(rs, photo, Allocation.MipmapControl.MIPMAP_NONE,
+                        Allocation.USAGE_SCRIPT);
+        final Allocation output = Allocation.createTyped(rs, input.getType());
+        final ScriptIntrinsicBlur script = ScriptIntrinsicBlur
+                .create(rs, Element.U8_4(rs));
+        script.setInput(input);
+        script.setRadius(15f);
+        script.forEach(output);
+        output.copyTo(photo);
+        return photo;
+    }
+
+    public static Bitmap createBlurImage(Context ctx, ImageView imageView) {
+        BitmapDrawable drawable = ((BitmapDrawable) imageView.getDrawable());
+        Bitmap bm = drawable.getBitmap();
+        return createBlurImage(ctx, bm.copy(Bitmap.Config.ARGB_8888, true));
+    }
+}
